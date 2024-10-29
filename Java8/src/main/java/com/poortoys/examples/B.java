07ce30e1b9b22d281b13865911ea9c4458b93d8f
abhujbal <abhujbal@csod.com>
2024-10-29 16:31:15 +0530

    Classes added.

---
+++ Java8/src/main/java/com/poortoys/examples/B.java
@@ -0,0 +1,11 @@
+package com.poortoys.examples;
+import com.poortoys.examples.p1.A;
+
+
+public class B {
+    public static void main(String[] args) {
+        A a = new A();
+        a.display();
+//        System.out.println(a.a);
+    }
+}
---
+++ Java8/src/main/java/com/poortoys/examples/p1/A.java
@@ -0,0 +1,11 @@
+//package com.poortoys.examples;
+package com.poortoys.examples.p1;
+public class A {
+        private int a=805;
+        public void display(){
+            System.out.println("Value of a: "+a);
+        }
+    public static void main(String[] args){
+
+    }
+}
