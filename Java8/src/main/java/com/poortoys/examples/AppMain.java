6190325d443f10c5831c7c34463b066922b9cdd3
abhujbal <abhujbal@csod.com>
2024-10-29 12:23:14 +0530

    Java8 added

--- Java8/src/main/java/com/poortoys/examples/AppMain.java
+++ Java8/src/main/java/com/poortoys/examples/AppMain.java
@@ -3,6 +3,9 @@ package com.poortoys.examples;
 public class AppMain {
 
     public static void main(String[] args) {
+
+        System.out.println("Hello, world");
+        System.out.println("This is an example Java program");
         System.out.println("Hello, world");
     }
 
